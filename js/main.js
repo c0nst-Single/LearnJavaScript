76a1e5964c2366114b17a41ef5acc697a5be2dab
Andrey <kayroswww@gmail.com>
2025-06-19 23:58:12 +0300

    39

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-function arg(arg1, arg2, arg3){
-    console.log(arguments)   //   все переданные аргументы попадут сформируются в массив
+function fnDec() {
+  return; //  declaration - можно вызывать до обьявления
 }
-arg('Make', 23, true)  // вывод -- Arguments(3) ['Make', 23, true]
+const fnExp = function () {
+  return; // expression - нельзя вызвать до обьявления
+};
 
-function fn(){
-    let total = 0
-    for(let i = 0; i < arguments.length;i++){ 
-        total = total + arguments[i];
-    }
-    return total;
+function arg(arg1, arg2, arg3) {
+  // 35
+  console.log(arguments); //   все переданные аргументы попадут сформируются в массивоподобный обьект
 }
-console.log(fn(10,20,15,45))    // применяется в случае когда неизвестно кол-во аргументов 
+arg("Make", 23, true); // вывод -- Arguments(3) ['Make', 23, true]
+
+function fn() {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total = total + arguments[i];
+  }
+  return total;
+}
+console.log(fn(10, 20, 15, 45)); // применяется в случае когда неизвестно кол-во аргументов
---
+++ js/main2.js
@@ -0,0 +1,10 @@
+// function pasRund(p) {
+//   const pass = "12fas0%8wNHAgqJ@g9c/vQE?SD4G>5cV!vx6WGzb7$pko3RghdmnbBrLSj^&X";
+//   let outPut = "";
+//   for (let i = 0; i < p; i++) {
+//     passRandom = Math.floor(Math.random() * pass.length);
+//     outPut = outPut + pass[passRandom];
+//   }
+//   return outPut;
+// }
+// console.log(pasRund(20));
