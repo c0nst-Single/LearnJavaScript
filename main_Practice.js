473adba6e9e2268c8fe4a44cdf372b4cbd8ebed3
Andrey <kayroswww@gmail.com>
2025-07-17 23:52:55 +0300

    126

--- main_Practice.js
+++ main_Practice.js
@@ -118,6 +118,59 @@ function BurgerMenu2() {
 
 // burger menu2-125 end
 
+// smooth__scrolling-126 start
+function smoothScroll() {
+  const navLinksHead = document.querySelectorAll(
+    ".smooth__scrolling-126 .nav-link"
+  );
+  function option1() {
+    for (let link of navLinksHead) {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const href = this.getAttribute("href");
+        // console.log(href);
+        document.querySelector(href).scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    }
+  }
+
+  function option2() {
+    navLinksHead.forEach((el) => {
+      el.addEventListener("click", function (e) {
+        e.preventDefault();
+        const href = this.getAttribute("href");
+        document.querySelector(href).scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    });
+  }
+  //option2();
+
+  function option3() {
+    document
+      .querySelector(".smooth__scrolling-126 .nav__ul")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        if (e.target.classList.contains("nav-link")) {
+          document
+            .querySelector(e.target.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+        }
+      });
+  }
+
+  document.getElementById("topRun").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".header").scrollIntoView({ behavior: "smooth" });
+  });
+  option3();
+}
+
+// smooth__scrolling-126 end
+
 // tab-127 start
 function tab127() {
   const buttons = document.querySelector(".tabs-127 .buttons");
@@ -147,6 +200,6 @@ function tab127() {
       .classList.remove("hidden");
   });
 }
-tab127();
+//tab127();
 
 // teb-127 end
